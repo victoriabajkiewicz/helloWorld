20eb811db35247612c378d71f6a9774108282b05
victoriabajkiewicz <152317574+victoriabajkiewicz@users.noreply.github.com>
2023-11-29 17:24:59 +0000

    added im on replit statement

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
          System.out.println("Hello world!");
          System.out.println("I love computer science");
          System.out.println("Hi");
+      System.out.println("I'm on replit :)");
 
     }
 }
